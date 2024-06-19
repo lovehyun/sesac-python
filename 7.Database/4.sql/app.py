5e25818d9af83d34bed2bcebca800a4460b0e181
Su-Hyun Park <lovehyun@hotmail.com>
2024-06-19 12:13:09 +0900

    로그인 페이지 구현중

--- 7.Database/4.sql/app.py
+++ 7.Database/4.sql/app.py
@@ -1,50 +1,11 @@
-from flask import Flask, render_template, request, session, redirect, url_for
+from flask import Flask, render_template, request, session, redirect, url_for, flash
 from datetime import timedelta
-import sqlite3
+import database as db
 
 app = Flask(__name__)
 app.secret_key = 'hello1234'
 app.permanent_session_lifetime = timedelta(minutes=5)
 
-# 사용자 DB
-DATABASE = 'users.db'
-
-def get_db_connection():
-    conn = sqlite3.connect(DATABASE)
-    conn.row_factory = sqlite3.Row # 행을 sqlite3.Row 라는 객체 타입으로 반환
-                                   # 이걸 설정하면 각 Row의 결과는 Dict 유형으로 반환됨.
-    return conn
-
-def init_db():
-    with app.app_context():
-        conn = get_db_connection()
-        cur = conn.cursor()
-        cur.execute('''
-            CREATE TABLE IF NOT EXISTS users (
-                    id INTEGER PRIMARY KEY AUTOINCREMENT,
-                    username TEXT NOT NULL,
-                    password TEXT NOT NULL)
-        ''')
-
-        # 기본 계정 추가 - 계정이 비었을때만...
-        cur.execute("SELECT COUNT(*) as count FROM users")
-        count = cur.fetchone()["count"]
-        # count = cur.fetchone()[0]  # 이건 Row 타입이 아닌 기본값 (즉 튜플로) 반납될때...
-        if count == 0:
-            cur.execute('INSERT INTO users (username, password) VALUES (?, ?)', ('user1', 'password1'))
-            cur.execute('INSERT INTO users (username, password) VALUES (?, ?)', ('user2', 'password2'))
-            conn.commit()
-
-        # 데이터 조회
-        cur.execute('SELECT * FROM users')
-        rows = cur.fetchall()
-
-        print('-' * 30)
-        for row in rows:
-            print(row['id'], row['username'], row['password']) # 열 이름을 사용(Dict) 해서 접근 가능함
-        print('-' * 30)
-
-        conn.close()
 
 @app.route('/')
 def home():
@@ -58,20 +19,19 @@ def login():
         password = request.form["password"]
 
         # 사용자 데이터를 외부 DB에서 가져오기
-        conn = get_db_connection()
-        cur = conn.cursor()
-        cur.execute("SELECT * FROM users WHERE username = ?", (username ,))
-        user_data = cur.fetchone()
+        # query = "SELECT * FROM users WHERE username = ? AND password = ?"
+        user_data = db.get_query("SELECT * FROM users WHERE username = ? AND password = ?", (username, password))
         print(f"조회된 사용자: ", user_data)
-        conn.close()
 
-        if user_data and user_data["password"] == password:
+        if len(user_data) == 1: # and user_data["password"] == password
             session["user"] = username
             session.permanent = True
             print("패스워드 맞음!!")
+            flash("로그인에 성공하였습니다.")
             return redirect(url_for("home"))
         else:
             print('패스워드 틀림!!')
+            flash("아이디/패스워드가 일치하지 않습니다.")
             # 메세지가 있으면 전달하고, 없으면 그냥 재 로그인
             return redirect(url_for("login"))
     else:
@@ -82,11 +42,25 @@ def login():
 
     return render_template('login.html')
 
+@app.route('/user', methods=['GET', 'POST'])
+def user():
+    if "user" in session:
+        user = session['user']
+
+        return render_template('user.html')
+
+@app.route('/view')
+def view():
+    users = [] # 미션1. DB 쿼리해서 모든 사용자 목록을 받아온다
+    return render_template("view.html", users=users)
+
 @app.route('/logout')
 def logout():
     session.pop("user", None)
+    flash("로그아웃에 성공하였습니다.")
     return redirect(url_for("login"))
 
 if __name__ == "__main__":
-    init_db()   # 시스템 부팅(?) 시에 DB 초기화 하기...
+    db.init_db()   # 시스템 부팅(?) 시에 DB 초기화 하기...
+    print('DB 초기화 완료')
     app.run(debug=True)  # 플라스크 앱 실행...
---
+++ 7.Database/4.sql/database.py
@@ -0,0 +1,55 @@
+import sqlite3
+
+# 사용자 DB
+DATABASE = 'users.db'
+
+def get_db_connection():
+    conn = sqlite3.connect(DATABASE)
+    conn.row_factory = sqlite3.Row # 행을 sqlite3.Row 라는 객체 타입으로 반환
+                                   # 이걸 설정하면 각 Row의 결과는 Dict 유형으로 반환됨.
+    return conn
+
+def get_query(query, params):
+    conn = get_db_connection()
+    cur = conn.cursor()
+    cur.execute(query, params)
+    result = cur.fetchall()
+    conn.close()
+    return result
+
+def execute_query(query, params):
+    conn = get_db_connection()
+    cur = conn.cursor()
+    cur.execute(query, params)
+    conn.commit()
+    conn.close()
+
+def init_db():
+    conn = get_db_connection()
+    cur = conn.cursor()
+    cur.execute('''
+        CREATE TABLE IF NOT EXISTS users (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                username TEXT NOT NULL,
+                password TEXT NOT NULL)
+    ''')
+
+    # 기본 계정 추가 - 계정이 비었을때만...
+    cur.execute("SELECT COUNT(*) as count FROM users")
+    count = cur.fetchone()["count"]
+    # count = cur.fetchone()[0]  # 이건 Row 타입이 아닌 기본값 (즉 튜플로) 반납될때...
+    if count == 0:
+        cur.execute('INSERT INTO users (username, password) VALUES (?, ?)', ('user1', 'password1'))
+        cur.execute('INSERT INTO users (username, password) VALUES (?, ?)', ('user2', 'password2'))
+        conn.commit()
+
+    # 데이터 조회
+    cur.execute('SELECT * FROM users')
+    rows = cur.fetchall()
+
+    print('-' * 30)
+    for row in rows:
+        print(row['id'], row['username'], row['password']) # 열 이름을 사용(Dict) 해서 접근 가능함
+    print('-' * 30)
+
+    conn.close()
